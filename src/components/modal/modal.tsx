05104323bf8b8b2b6dfe584ec62bc843fcd20931
tz2015-tatiana <tz2015@hw.ac.uk>
2024-05-23 17:19:16 +0400

    add ModalOverlay to Modal component

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import modalStyles from './modal.module.css';
 import BurgerIngredient from "../../utils/ingredient-interface";
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import ModalOverlay from "../modal-overlay/modal-overlay";
 
 const modalRoot = document.getElementById("modal-root");
 
@@ -28,17 +29,17 @@ function Modal(props: ModalProps) {
     return () => document.removeEventListener('keydown', handleEscapeKey);
   }, [props.onClose]);
 
-  return ReactDOM.createPortal(
-    (
+  return ReactDOM.createPortal((
+    <ModalOverlay onClose={props.onClose}>
       <div className={`${modalStyles.modal} pt-10 pb-10 pr-8 pl-8`}>
         <div className={ modalStyles.headerContainer }>
           <h2 className="text text_type_main-large">{ props.title }</h2>
           <CloseIcon type="primary" onClick={ props.onClose }/>
         </div>
         {props.children} 
-      </div>
-    ), modalRoot!
-  );
+      </div> 
+    </ModalOverlay>), 
+    modalRoot!);
 }
 
 export default Modal;
