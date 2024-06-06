457abb9573ab9ef02e43fdead4487a4666c8af4e
tz2015-tatiana <tz2015@hw.ac.uk>
2024-06-06 11:00:35 +0400

    deleted actions from services/burger-constructor, deleted comments from burger-constructor/reducer

--- src/services/burger-constructor/reducer.tsx
+++ src/services/burger-constructor/reducer.tsx
@@ -53,32 +53,3 @@ export const burgerConstructorSlice = createSlice({
 })
 
 export const { addBun, addIngredient, moveIngredient, removeIngredient, resetConstructor } = burgerConstructorSlice.actions;
-
-// export const burgerConstructorSlice = createSlice({
-//   name: "burgerConstructor",
-//   initialState,
-//   reducers: {},
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(addBun.fulfilled, (state, action) => {
-//         state.bun = action.payload;
-//       })
-//       .addCase(addIngredient.fulfilled, (state, action) => {
-//         state.fillings.push(action.payload);
-//       })
-//       .addCase(removeIngredient.fulfilled, (state, action) => {
-//         state.fillings = state.fillings.filter(item => item._id !== action.payload);
-//       })
-//       .addCase(moveIngredient.fulfilled, (state, action) => {
-//         const {dragIndex, hoverIndex} = action.payload;
-//         const updatedIngredients = [...state.fillings];
-//         const movedItem = updatedIngredients.splice(dragIndex, 1)[0];
-//         updatedIngredients.splice(hoverIndex, 0, movedItem);
-//         state.fillings = updatedIngredients;
-//       })
-//       .addCase(resetConstructor.fulfilled, (state) => {
-//         state.bun = null;
-//         state.fillings = [];
-//       })
-//   }
-// })
