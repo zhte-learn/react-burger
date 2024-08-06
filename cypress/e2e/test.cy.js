2ec38d5bae9c84953e5ab2ea981ffab06d2f2899
tz2015-tatiana <tz2015@hw.ac.uk>
2024-08-06 13:36:50 +0400

    removed text duplication by creating constants

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,6 +1,11 @@
-const bunId = '643d69a5c3f7b9001cfa093c'; // "Краторная булка N-200i"
-const ingredientId1 = '643d69a5c3f7b9001cfa0941'; // "Биокотлета из марсианской Магнолии"
-const ingredientId2 = "643d69a5c3f7b9001cfa0942"; // "Соус Spicy-X"
+const bunId = '643d69a5c3f7b9001cfa093c';
+const bunName = 'Краторная булка N-200i';
+const mainIngredientId = '643d69a5c3f7b9001cfa0941';
+const mainIngredientName = 'Биокотлета из марсианской Магнолии';
+const sauceId = '643d69a5c3f7b9001cfa0942';
+const sauceName = 'Соус Spicy-X';
+const newBunId = '643d69a5c3f7b9001cfa093d';
+const newBunName = 'Флюоресцентная булка R2-D3';
 
 const modalClass = '[class^="modal_modal"]';
 const ingredientItemClass = "ingredient-item";
@@ -16,26 +21,25 @@ describe('Application', () => {
   it("should display the same ingredient data in the modal as the one clicked", () => {
     cy.get(`[data-testid="${ingredientItemClass}-${bunId}"]`).click();
     cy.checkModalIsVisible(modalClass);
-    cy.get('[class^="ingredient-details_title"]').should("have.text", "Краторная булка N-200i");
+    cy.get('[class^="ingredient-details_title"]').should("have.text", bunName);
   });
 
   it("should close modal as close button is clicked", () => {
-    cy.get(`[data-testid="${ingredientItemClass}-${ingredientId1}"]`).click();
+    cy.get(`[data-testid="${ingredientItemClass}-${mainIngredientId}"]`).click();
     cy.checkModalIsVisible(modalClass);
     cy.get('[class^="modal_closeIcon"]').click();
     cy.checkModalIsNotExist(modalClass);
   });
 
   it("should close modal as ESC button is pressed", () => {
-    const ingredientId = '643d69a5c3f7b9001cfa0941'; // "Биокотлета из марсианской Магнолии"
-    cy.get(`[data-testid="${ingredientItemClass}-${ingredientId}"]`).click();
+    cy.get(`[data-testid="${ingredientItemClass}-${mainIngredientId}"]`).click();
     cy.checkModalIsVisible(modalClass);
     cy.get('body').type('{esc}');
     cy.checkModalIsNotExist(modalClass);
   });
 
   it("should close modal as user clicked on overlay", () => {
-    cy.get(`[data-testid="${ingredientItemClass}-${ingredientId1}"]`).click();
+    cy.get(`[data-testid="${ingredientItemClass}-${mainIngredientId}"]`).click();
     cy.checkModalIsVisible(modalClass);
     // click on a specific part of the overlay to close the modal
     // because overlay and modal are overlaps
@@ -44,11 +48,11 @@ describe('Application', () => {
   });
 
   it("should create order with drag element and drop when user is logged out", () => {
-    cy.prepareBurger3(bunId, ingredientId1, ingredientId2);
-    cy.checkIngredientsAdded("Краторная булка N-200i", "Биокотлета из марсианской Магнолии", "Соус Spicy-X");
-    cy.checkCounterValue("Краторная булка N-200i", "2");
-    cy.checkCounterValue("Биокотлета из марсианской Магнолии", "1");
-    cy.checkCounterValue("Соус Spicy-X", "1");
+    cy.prepareBurger3(bunId, mainIngredientId, sauceId);
+    cy.checkIngredientsAdded(bunName, mainIngredientName, sauceName);
+    cy.checkCounterValue(bunName, "2");
+    cy.checkCounterValue(mainIngredientName, "1");
+    cy.checkCounterValue(sauceName, "1");
     cy.get(buttonConfirmClass).click();
     cy.prepareLogin("cat@mail.ru", "123");
     cy.intercept("POST", "orders", { fixture: "orders" });
@@ -60,11 +64,11 @@ describe('Application', () => {
   it("should create order with drag element and drop when user is logged in", () => {
     cy.visit('login');
     cy.prepareLogin("cat@mail.ru", "123");
-    cy.prepareBurger3(bunId, ingredientId1, ingredientId2);
-    cy.checkIngredientsAdded("Краторная булка N-200i", "Биокотлета из марсианской Магнолии", "Соус Spicy-X");
-    cy.checkCounterValue("Краторная булка N-200i", "2");
-    cy.checkCounterValue("Биокотлета из марсианской Магнолии", "1");
-    cy.checkCounterValue("Соус Spicy-X", "1");
+    cy.prepareBurger3(bunId, mainIngredientId, sauceId);
+    cy.checkIngredientsAdded(bunName, mainIngredientName, sauceName);
+    cy.checkCounterValue(bunName, "2");
+    cy.checkCounterValue(mainIngredientName, "1");
+    cy.checkCounterValue(sauceName, "1");
     cy.intercept("POST", "orders", { fixture: "orders" });
     cy.intercept("POST", "token", { fixture: "token" });
     cy.get(buttonConfirmClass).click();
@@ -73,15 +77,15 @@ describe('Application', () => {
   });
 
   it("should allow to delete an ingredient by clicking on delete button", () => {
-    cy.prepareBurger3(bunId, ingredientId1, ingredientId2);
-    cy.checkIngredientsAdded("Краторная булка N-200i", "Биокотлета из марсианской Магнолии", "Соус Spicy-X");
-    cy.checkCounterValue("Краторная булка N-200i", "2");
-    cy.checkCounterValue("Биокотлета из марсианской Магнолии", "1");
-    cy.checkCounterValue("Соус Spicy-X", "1");
+    cy.prepareBurger3(bunId, mainIngredientId, sauceId);
+    cy.checkIngredientsAdded(bunName, mainIngredientName, sauceName);
+    cy.checkCounterValue(bunName, "2");
+    cy.checkCounterValue(mainIngredientName, "1");
+    cy.checkCounterValue(sauceName, "1");
 
     //find delete button on "Соус Spicy-X"
     cy.get('[class^="burger-constructor_list"]')
-      .contains("Соус Spicy-X")
+      .contains(sauceName)
       .closest('[class^="constructor-item_item"]')
       .find('.delete-lock-item')
       .click();
@@ -90,22 +94,20 @@ describe('Application', () => {
     cy.get('[class^="burger-constructor_list"]')
       .find('[class^="constructor-item_item"]')
       .each(($el) => {
-        cy.wrap($el).find('[class^="constructor-item_name"]').should("not.contain", "Соус Spicy-X");
+        cy.wrap($el).find('[class^="constructor-item_name"]').should("not.contain", sauceName);
       })
     
-    cy.checkCounterNotExist("Соус Spicy-X");
+    cy.checkCounterNotExist(sauceName);
   })
 
   it("should replace a bun in constructor when a new bun is added", () => {
-    cy.prepareBurger3(bunId, ingredientId1, ingredientId2);
-    cy.checkIngredientsAdded("Краторная булка N-200i", "Биокотлета из марсианской Магнолии", "Соус Spicy-X");
-    
-    const newBun = "643d69a5c3f7b9001cfa093d"; //"Флюоресцентная булка R2-D3"
-    cy.get(`[data-testid="${ingredientItemClass}-${newBun}"]`).trigger("dragstart");
+    cy.prepareBurger3(bunId, mainIngredientId, sauceId);
+    cy.checkIngredientsAdded(bunName, mainIngredientName, sauceName);
+    cy.get(`[data-testid="${ingredientItemClass}-${newBunId}"]`).trigger("dragstart");
     cy.get("[data-testid=drop-zone-bun]").first().trigger("drop");
 
-    cy.checkIngredientsAdded("Флюоресцентная булка R2-D3", "Биокотлета из марсианской Магнолии", "Соус Spicy-X");
-    cy.checkCounterNotExist("Краторная булка N-200i");
-    cy.checkCounterValue("Флюоресцентная булка R2-D3", "2");
+    cy.checkIngredientsAdded(newBunName, mainIngredientName, sauceName);
+    cy.checkCounterNotExist(bunName);
+    cy.checkCounterValue(newBunName, "2");
   })
 })
